302b3ed1ed0f97575deb132b9cb6cd10b658c69f
chexedy <106210798+chexedy@users.noreply.github.com>
2025-09-13 15:02:40 -0400

    got markers to work for SEC and NWP

--- js/map.js
+++ js/map.js
@@ -2,33 +2,61 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXlhYW43bSIsImEiOiJjbWZoZTA1c3IwYjg2MnNxMnB6Y
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-	center: [-74.5, 40], // starting position [lng, lat]
-	zoom: 9, // starting zoom
+    center: [-74.1, 40.75], // starting position [lng, lat]
+    zoom: 10, // starting zoom
     minZoom: 8,
     style: "mapbox://styles/ayaan7m/cmfhdj6gw006i01qu2774d2nz",
 });
 
-stations = {
-    "Secaucus Junction" : [-74.07575, 40.76131],
-    "Newark Penn Station" : [-74.16413, 40.73438],
-}
+fetch('./json/stations.json')
+    .then(res => res.json())
+    .then(data => {
+        map.on('load', () => {
+            // Load your custom image
+            map.loadImage("../assets/icons/service/station.png", (error, image) => {
+                if (error) throw error;
 
-map.on("load", () => {
-    for (const key in stations) {
-        map.loadImage("../assets/icons/service/station.png"), (error, image) => {
-            if (error) throw error;
-
-            map.addImage(key, image);
-            map.addSource(key + "source", {
-                "type" : "geojson",
-                "data" : {
-                    "type" : "Feature",
-                    "geometry" : {
-                        "type" : "Point",
-                        "coordinates" : stations[key],
-                    }
+                // Add the image to the map style
+                if (!map.hasImage('station-icon')) {
+                    map.addImage('station-icon', image);
                 }
-            })
-        }    
-    }
-})
+
+                // Add GeoJSON source
+                map.addSource('stations', {
+                    type: 'geojson',
+                    data: data.stations
+                });
+
+                // Add a symbol layer using the custom icon
+                map.addLayer({
+                    id: 'stations-layer',
+                    type: 'symbol',
+                    source: 'stations',
+                    layout: {
+                        'icon-image': 'station-icon',
+                        'icon-size': 0.1, // adjust to make it bigger/smaller
+                        'icon-allow-overlap': true,
+                        'text-field': ['get', 'description'], // optional label
+                        'text-offset': [0, 1.2],
+                        'text-anchor': 'top'
+                    }
+                });
+
+                // Add popup on click
+                map.on('click', 'stations-layer', (e) => {
+                    const props = e.features[0].properties;
+                    new mapboxgl.Popup()
+                        .setLngLat(e.lngLat)
+                        .setHTML(`<h3>${props.title}</h3><p>${props.description}</p>`)
+                        .addTo(map);
+                });
+
+                map.on('mouseenter', 'stations-layer', () => {
+                    map.getCanvas().style.cursor = 'pointer';
+                });
+                map.on('mouseleave', 'stations-layer', () => {
+                    map.getCanvas().style.cursor = '';
+                });
+            });
+        });
+    });
